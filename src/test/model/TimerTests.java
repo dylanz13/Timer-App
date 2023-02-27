2e61dbd2e1b33ed55c9c52616f9a7c53c4f43ef9
dyzha <xzhan185@student.ubc.ca>
2023-02-26 20:00:22 -0800

    Improved TimerTests.java test coverage.

--- src/test/model/TimerTests.java
+++ src/test/model/TimerTests.java
@@ -34,10 +34,59 @@ public class TimerTests {
     }
 
     @Test
-    public void testStart() {
+    public void testStartNoSubjects() {
         t.start(new ArrayList<>(), new ArrayList<>());
         assertTrue(t.isStopped());
+    }
 
+    @Test
+    public void testStartCompleteSubjects() {
+        ArrayList<Subject> s = new ArrayList<>();
+        ArrayList<Subject> cs = new ArrayList<>();
+
+        s.add(new Subject("Math", 10));
+        s.add(new Subject("CPSC", 10));
+
+        t.start(s, cs);
+
+        ArrayList<Subject> rcs = new ArrayList<>();
+
+        rcs.add(new Subject("Math", 0));
+        rcs.add(new Subject("CPSC", 0));
+        assertTrue(s.isEmpty());
+        int i = 0;
+        for (Subject c : cs) {
+            assertEquals(rcs.get(i).getDescription(), c.getDescription());
+            assertEquals(rcs.get(i).getSecondsRemaining(), c.getSecondsRemaining());
+            i++;
+        }
+    }
+
+    @Test
+    public void testStartIncompleteSubjects() {
+        ArrayList<Subject> s = new ArrayList<>();
+        ArrayList<Subject> cs = new ArrayList<>();
+
+        s.add(new Subject("Math", 10));
+        s.add(new Subject("English", 2));
+        s.add(new Subject("CPSC", 10));
+
+        t.start(s, cs);
+
+        ArrayList<Subject> rs = new ArrayList<>();
+        ArrayList<Subject> rcs = new ArrayList<>();
+
+        rs.add(new Subject("CPSC", 2));
+        rcs.add(new Subject("Math", 0));
+        rcs.add(new Subject("English", 0));
+        assertEquals(rs.get(0).getDescription(), s.get(0).getDescription());
+        assertEquals(rs.get(0).getSecondsRemaining(), s.get(0).getSecondsRemaining());
+        int i = 0;
+        for (Subject c : cs) {
+            assertEquals(rcs.get(i).getDescription(), c.getDescription());
+            assertEquals(rcs.get(i).getSecondsRemaining(), c.getSecondsRemaining());
+            i++;
+        }
 
     }
 }
